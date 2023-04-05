d5a34174fcc77263df897eb63f6c514ece865d36
Philip Chestnykh <p.chestnykh@dreamteam-mobile.com>
2023-04-06 03:31:15 +1200

    Migrate to Xamarin.Vonage.Android -> 2.24.2

--- lib/Xamarin.Forms.Vonage.Android/Services/VonageSessionOptions.cs
+++ lib/Xamarin.Forms.Vonage.Android/Services/VonageSessionOptions.cs
@@ -4,7 +4,7 @@ namespace Xamarin.Forms.Vonage.Android.Services
 {
     internal sealed class VonageSessionOptions : Session.SessionOptions
     {
-        public override bool IsCamera2Capable => true;
+        public bool IsCamera2Capable => true;
 
         public override bool UseTextureViews() => true;
     }
